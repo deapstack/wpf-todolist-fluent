33a164a3fe730a3afc48b0bcca53f6ee2cf096a6
sam <samuel.alves-nunes@cegelec.com>
2025-08-31 14:16:54 +0200

    Initial version

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.VisualBasic;
+using System.Collections;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +13,45 @@ using System.Windows.Shapes;
 
 namespace TodoList
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            lvEntries.Items.Add("Hello");
+            lvEntries.Items.Add("Hello2");
+            lvEntries.Items.Add("Hello3");
+            lvEntries.Items.Add("Hello4");
+        }
+
+        private void btnAddItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbItemToAdd.Text.Length > 0)
+            {
+                lvEntries.Items.Add(tbItemToAdd.Text);
+                tbItemToAdd.Clear();
+            }
+        }
+        private void btnRemoveItem_Click(object sender, RoutedEventArgs e)
+        {
+            var items = lvEntries.SelectedItems;
+            var result = MessageBox.Show($"Delete {items.Count} items ?", "Confirm action", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                List<string> itemsListCopy = items.OfType<string>().ToList();
+                foreach (var item in itemsListCopy)
+                {
+                    lvEntries.Items.Remove(item);
+                }
+            }
+        }
+        private void btnClearAllItems_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show($"Delete all items ?", "Confirm action", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                lvEntries.Items.Clear();
+                }
         }
     }
 }
